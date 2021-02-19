20ed92503c6ca0ba796f3931df07f23d23b4bf82
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-02-19 10:34:59 -0700

    Deleting an Element List

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -14,13 +14,17 @@ const Ingredients = () => {
     ]);
   };
 
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+  }
+
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} onRemoveItem={()=>{}} />
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
