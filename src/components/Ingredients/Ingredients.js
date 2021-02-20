3c4e513aa0e7009031ad62825a4943999833d195
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-02-19 21:00:32 -0700

    More on useEffect()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -27,6 +27,10 @@ const Ingredients = () => {
     console.log('Rendering ingredients', userIngredients);
   }, [userIngredients]);
 
+  const filteredIngredientsHandler = filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }
+
   const addIngredientHandler = ingredient => {
     fetch('https://reactapp-hooks-demo-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
@@ -50,7 +54,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
