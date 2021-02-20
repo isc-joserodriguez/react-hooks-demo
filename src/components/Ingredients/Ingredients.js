88f84d03b720298bdc77e09a78b211945815d14b
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-02-20 00:43:52 -0700

    Deleting Ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -31,7 +31,11 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+    fetch(`https://reactapp-hooks-demo-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
+      method: 'DELETE',
+    }).then(response => {
+      setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+    });
   }
 
   return (
